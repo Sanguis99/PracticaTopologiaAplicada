c65ff0676ca0929a3df5b5afdd356ad2445eb52b
jorge <jorge.ramos.garcia@alumnos.upm.es>
2025-12-07 19:49:28 +0100

    Me he equivocado, lo he vuelto a cambiar a como estaba lo de show voronoi

--- simplices.py
+++ simplices.py
@@ -463,7 +463,6 @@ class Punto:
 class Complejo_Vietoris_Rips:
     def __init__(self, points):
         self.puntos = points  # points es una lista de objetos Punto
-        self.coords_puntos = np.array([p.coords for p in points])  # matriz de coordenadas
         self.space_dimension = len(points[0].coords) if points else 0 # Dimensión del espacio en el que están los puntos
 
     # Funcion para calcular el r-complejo de Vietori-Rips
@@ -493,20 +492,6 @@ class Complejo_Vietoris_Rips:
 
     def vertices(self, puntos):
         return [p.vertice for p in puntos]
-
-    # Funcion para mostrar voronoi y Delaunay copiada de los ejemplos
-    def show_voronoi_delaunay(self):
-        # Usamos la librería scipy.spatial para Voronoi, Delaunay y voronoi_plot_2d
-        vor = Voronoi(self.coords_puntos)
-        Del = Delaunay(self.coords_puntos)
-        fig = voronoi_plot_2d(vor,show_vertices=False,line_width=2, line_colors='blue' )
-        c=np.ones(len(self.coords_puntos))
-        cmap = matplotlib.colors.ListedColormap("limegreen")
-        plt.tripcolor(self.coords_puntos[:,0],self.coords_puntos[:,1],Del.simplices, c, edgecolor="k", lw=2,
-        cmap=cmap)
-        plt.plot(self.coords_puntos[:,0], self.coords_puntos[:,1], 'ko')
-        plt.show()
-
 
 # Debemos crearnos una función que calcule la filtración de alfa-complejos asociada a un conjunto de puntos en el plano
 class AlfaComplejo:
@@ -573,6 +558,19 @@ class AlfaComplejo:
         complejo = Complejo_simplicial_filtrado([])
         complejo.insert_filtrado(s_aux, r)
         return complejo
+
+    # Funcion para mostrar voronoi y Delaunay copiada de los ejemplos
+    def show_voronoi_delaunay(self):
+        # Usamos la librería scipy.spatial para Voronoi, Delaunay y voronoi_plot_2d
+        vor = Voronoi(self.coords_puntos)
+        Del = Delaunay(self.coords_puntos)
+        fig = voronoi_plot_2d(vor,show_vertices=False,line_width=2, line_colors='blue' )
+        c=np.ones(len(self.coords_puntos))
+        cmap = matplotlib.colors.ListedColormap("limegreen")
+        plt.tripcolor(self.coords_puntos[:,0],self.coords_puntos[:,1],Del.simplices, c, edgecolor="k", lw=2,
+        cmap=cmap)
+        plt.plot(self.coords_puntos[:,0], self.coords_puntos[:,1], 'ko')
+        plt.show()
 
     # Dibuja el diagrama de Voronoi junto con el alfa-complejo
     def show_voronoi_alfa(self):
